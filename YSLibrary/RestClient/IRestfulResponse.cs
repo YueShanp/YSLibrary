68d497fee18327ad94936c2dd9ba311586496ae5
livebreeze <livebreeze@gmail.com>
2017-01-21 11:27:51 +0800

    add  IRestfulResponse

---
+++ YSLibrary/RestClient/IRestfulResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace YSLibrary.RestClient
+{
+    public interface IRestfulResponse
+    {
+        string RequestID { get; set; }
+
+        string RequestedUrl { get; set; }
+
+        bool IsSuccessStatusCode { get; set; }
+
+        HttpStatusCode StatusCode { get; set; }
+
+        string MessageCode { get; set; }
+
+        string[] ReasonPhrase { get; set; }
+    }
+}
